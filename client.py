bb7c8c00e561833739ce0a43248de4aeb4f4b35c
clover <edik22217@gmail.com>
2024-03-06 20:26:37 +0400

    client registration

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 import socket
 
-while True:
+
+def request(req: str):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('192.168.103.25', 8888))
 
     print('!connected')
 
-    client.send(input().encode('utf-8'))
+    client.send(req.encode('utf-8'))
 
     data = client.recv(1024)
     print(f"Message: {data.decode('utf-8')}")
--- main.py
+++ main.py
@@ -1,20 +1,45 @@
 # библиотеки
 import sys
-from PyQt5 import QtGui, QtCore
+from PyQt5 import QtGui, QtCore, uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt, QSize
 from screeninfo import get_monitors
 
-from Enter import Ui_MainWindow as Enter
-from Registration import Ui_MainWindow as Registration
+from client import request
+
+
+class PasswordError(Exception):
+    pass
+
+
+class SymbolError(Exception):
+    pass
+
+
+class HardError(Exception):
+    pass
+
+
+class GenderError(Exception):
+    pass
+
+
+def error_box(msg):
+    error = QMessageBox()
+    error.setWindowTitle('Ошибка')
+    error.setText(msg)
+    error.setIcon(QMessageBox.Warning)
+    error.exec()
 
 
 # класс Главного_Окна
-class EnterWidget(QMainWindow, Enter):
+class EnterWidget(QMainWindow):
     # инициализация Главного_Окна
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
+        super(EnterWidget, self).__init__()
+        uic.loadUi('Data/Ui_files/Enter.ui', self)
+        self.show()
+        self.reg = None
         # кнопки
         self.registration.clicked.connect(self.go_registration)
 
@@ -23,14 +48,44 @@ class EnterWidget(QMainWindow, Enter):
             self.reg = RegWidget()
             self.reg.show()
             self.close()
-        except Exception:
-            print("go_registration_error")
+        except Exception as e:
+            print(e)
 
 
-class RegWidget(QMainWindow, Registration):
+class RegWidget(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.setupUi(self)
+        super(RegWidget, self).__init__()
+        uic.loadUi('Data/Ui_files/Registration.ui', self)
+        self.show()
+        self.registration.clicked.connect(self.registrate)
+
+    def registrate(self):
+        try:
+            name = self.login.text()
+            password = self.password.text()
+            c_password = self.password1.text()
+            if password != c_password:
+                raise PasswordError
+            if self.M_radio.isChecked():
+                gender = '1'
+            elif self.F_radio.isChecked():
+                gender = '0'
+            else:
+                raise GenderError
+            ans = request(f'r! {name}!{password}!{gender}')
+            if ans != '!Success':
+                error_box(ans[1:])
+        except PasswordError:
+            error_box('Пароли не совпадают')
+        except GenderError:
+            error_box('Гендер не выбран')
+        except SymbolError:
+            error_box('Использованы недопустимые символы')
+        except HardError:
+            error_box('Пароль слишком простой')
+        except Exception as e:
+            error_box('Произошла непредвиденная ошибка')
+            print(e)
 
 
 # запуск
