e49a420ad5a04e4e38e354eee13afb95756aa6c4
clover <edik22217@gmail.com>
2024-03-06 15:07:08 +0400

    client dialog

--- client.py
+++ client.py
@@ -3,8 +3,12 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('192.168.103.25', 8888))
 
-while True:
-    data = client.recv(1024)
-    print(data.decode('utf-8'))
+print('!connected')
 
+while True:
     client.send(input().encode('utf-8'))
+
+    data = client.recv(1024)
+    print(f"Message: {data.decode('utf-8')}")
+
+
