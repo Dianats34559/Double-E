71488c093bc477e2429dfb1743c0df799a53b4d9
clover <edik22217@gmail.com>
2024-03-06 14:34:26 +0400

    begin server

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# Это СЕРВЕР
+import socket
+
+server = socket.create_server(('192.168.103.25', 8888))
+
+while True:
+    server.listen()
+    print('listen...')
+    print('')
+    client, address = server.accept()
+    print('client connected')
+    print('')
+    data = client.recv(1024).decode('utf-8')
+    print(data)
+    client.send("you are welcome".encode('utf-8'))
+    print('Well done!')
