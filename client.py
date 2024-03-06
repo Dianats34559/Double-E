546d1dfb9f344164c15a6393db48736c4bb92051
CTmobil <92884893+CTmobil@users.noreply.github.com>
2024-03-06 15:54:51 +0400

    re-client

--- client.py
+++ client.py
@@ -1,14 +1,16 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.103.25', 8888))
-
-print('!connected')
-
 while True:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('192.168.103.25', 8888))
+
+    print('!connected')
+
     client.send(input().encode('utf-8'))
 
     data = client.recv(1024)
     print(f"Message: {data.decode('utf-8')}")
+
+    client.close()
 
 
